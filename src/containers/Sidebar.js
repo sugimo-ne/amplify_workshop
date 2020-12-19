11c1029d26e59e586b386182a8ce39af91af6786
sugimo-ne <kein.sugimoto@gmail.com>
2020-12-19 21:42:13 +0900

    Edit Logout Button text

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -187,7 +187,7 @@ export default function Sidebar({ activeListItem }) {
           <ListItemText
             primary={
               <Button variant="outlined" onClick={signOut} fullWidth>
-                Sign out
+                ログアウト
               </Button>
             }
           />
